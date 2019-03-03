6d03013ecdd187fc8ecde300a88acb044e91d7a5
Lucas <ldoliveira91@outlook.com>
2019-03-03 19:00:35 -0300

    adding preloading strategy to loading components in background on startup the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -37,7 +37,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     HttpModule,
     SharedModule.forRoot(),
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'pt-BR'}
